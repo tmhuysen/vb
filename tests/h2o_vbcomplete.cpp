303c6d2c997ce18fb2424d950568cd3de66474f0
Tm Huysen <tobias.huysentruyt@minimoa.net>
2018-06-26 13:49:17 +0200

    finish clean-up

--- tests/h2o_vbcomplete.cpp
+++ tests/h2o_vbcomplete.cpp
@@ -22,29 +22,14 @@ BOOST_AUTO_TEST_CASE ( H2O_complete_vb ) {
 
 }
 
-BOOST_AUTO_TEST_CASE ( H2O_complete_vb_test_output ) {
-    // Psi4 and GAMESS' FCI energy
-    double reference_energy = -75.0129803939602;
 
-    libwint::Molecule H2O("../../tests/data/h2o.xyz");
-    libwint::AOBasis ao_basis(H2O,"STO-3G");
-    ao_basis.calculateIntegrals();
-    vb::CompleteVB vb (ao_basis,5,5);
-
-    double internuclear_repulsion = H2O.calculateInternuclearRepulsionEnergy();
-    double energy = vb.solve() + internuclear_repulsion;
-
-    BOOST_CHECK(std::abs(energy-reference_energy)<1.0e-6);
-
-}
-
-BOOST_AUTO_TEST_CASE ( was_er_gebreut ) {
+BOOST_AUTO_TEST_CASE ( H4_ref_visual ) {
     // generate reference H4
 
-    libwint::Molecule H4("../../tests/data/h6.xyz");
+    libwint::Molecule H4("../../tests/data/h4.xyz");
     libwint::AOBasis ao_basis(H4,"STO-3G");
     ao_basis.calculateIntegrals();
-    vb::CompleteVB vb (ao_basis,3,3);
+    vb::CompleteVB vb (ao_basis,2,2);
 
     double internuclear_repulsion = H4.calculateInternuclearRepulsionEnergy();
     double energy = vb.solve() + internuclear_repulsion;
--- tests/selectiveVB_tests.cpp
+++ tests/selectiveVB_tests.cpp
@@ -8,9 +8,8 @@
 
 
 BOOST_AUTO_TEST_CASE ( Heitler_london ) {
-
-    // Visual test
-
+    // Cristina's H2 FCI energy/OO-DOCI energy
+    double reference_energy = -1.13726;
     vb::State cov{{1,2},{2,1},{1,1}};
     vb::State ion1{{1,2},{1,2},{1,1}};
     libwint::Molecule H2("../../tests/data/h2.xyz");
@@ -18,45 +17,8 @@ BOOST_AUTO_TEST_CASE ( Heitler_london ) {
     ao_basis.calculateIntegrals();
     std::vector<vb::State> states = {cov,ion1};
     vb::SelectiveVB elective_amnesia(ao_basis.get_T()+ao_basis.get_V(),ao_basis.get_g(),ao_basis.get_S(),states);
-
-    std::cout<<elective_amnesia.solve()+H2.calculateInternuclearRepulsionEnergy();
-    std::cout<<std::endl;
-    std::cout<<elective_amnesia.get_eigenvectors();
-
-
-
-
-
+    double energy = elective_amnesia.solve()+H2.calculateInternuclearRepulsionEnergy();
+    BOOST_CHECK(std::abs(energy-reference_energy)<1.0e-6);
 
 }
 
-BOOST_AUTO_TEST_CASE ( Heitler_london_H6 ) {
-    // H6 calculation of "all" determinants
-    libwint::Molecule H4("../../tests/data/h6.xyz");
-    libwint::AOBasis ao_basis(H4,"STO-3G");
-    ao_basis.calculateIntegrals();
-    std::vector<vb::State> states = {};
-    std::vector<size_t> tater{7};
-    size_t det=7;
-    for(int i =0;i<19;i++){
-        det = vb::next_bitset_permutation(det);
-        tater.push_back(det);
-
-    }
-    for(int i=0;i<20;i++){
-        for(int j=0;j<20;j++){
-            vb::State dummy{{tater[i]},{tater[j]},{1}};
-            states.push_back(dummy);
-
-        }
-    }
-    std::cout<<std::endl<<states.size()<<std::endl;
-    vb::SelectiveVB elective_amnesia(ao_basis.get_T()+ao_basis.get_V(),ao_basis.get_g(),ao_basis.get_S(),states);
-
-    std::cout<<std::endl<<elective_amnesia.solve()+H4.calculateInternuclearRepulsionEnergy()<<std::endl;
-
-
-
-
-
-}
